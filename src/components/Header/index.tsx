678c0463219acb57f5a1c4bad0d2ab4889b5cec0
vladji <vladjislav@gmail.com>
2022-10-13 21:01:01 +0600

    fix: menu link visited color

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { selectIsMobile } from "../../state/common/selectors";
 import HeaderDesktop from "./Header";
-import HeaderMobile from "./Mobile/Header";
+import HeaderMobile from "./mobile/Header";
 
 const Header = (): JSX.Element => {
   const isMobile = useSelector<boolean>(selectIsMobile);
