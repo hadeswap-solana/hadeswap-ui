423410429db5655e3e1f3d2452124081b6c37f02
vladji <vladjislav@gmail.com>
2022-11-12 15:20:32 +0600

    refactor: collection / pool item

--- src/components/ColletionItem/mobile/index.tsx
+++ src/components/ColletionItem/mobile/index.tsx
@@ -1,20 +1,13 @@
 import { FC } from 'react';
-import { BN } from 'hadeswap-sdk';
-import {
-  COLLECTION_ITEM,
-  POOL_ITEM,
-  OWNER_PUBLIC_KEY,
-  TOTAL_ACCUMULATED_FEES,
-} from '../constants';
+import { COLLECTION_ITEM, POOL_ITEM } from '../constants';
 import { UNTITLED_COLLECTION, COLLECTION } from '../../../constants/common';
-import { formatBNToString } from '../../../utils';
-import { shortenAddress } from '../../../utils/solanaUtils';
-import { PriceWithIcon } from '../../PriceWithIcon';
+import { createPoolTableRow } from '../../../state/core/helpers';
+import { MarketInfo } from '../../../state/core/types';
 
 import styles from './CollectionItem.module.scss';
 
 interface CollectionItemsProps {
-  item: any;
+  item: ReturnType<typeof createPoolTableRow> | MarketInfo;
   onRowClick: (arg: string) => void;
   listingType?: string;
 }
@@ -38,38 +31,12 @@ const CollectionItem: FC<CollectionItemsProps> = ({
       </div>
       <div className={styles.cardBody}>
         <ul>
-          {itemMap.list.map((listItem) => {
-            let value = item[listItem.valueKey];
-
-            if (listItem.valueKey === OWNER_PUBLIC_KEY) {
-              value = shortenAddress(value);
-            }
-
-            if (listItem.valueKey === TOTAL_ACCUMULATED_FEES) {
-              value =
-                item.type === 'liquidity provision'
-                  ? formatBNToString(new BN(value || '0'))
-                  : '--';
-            }
-
-            if (item.type === 'tokenForNft') {
-              value = '--';
-            }
-
-            return (
-              <li key={listItem.valueKey} className={styles.listItem}>
-                <span>{listItem.title}</span>
-                {listItem.price && !isNaN(value) ? (
-                  <PriceWithIcon price={value ? value : '0'} rightIcon />
-                ) : (
-                  <span>
-                    {value}
-                    {listItem.percent && '%'}
-                  </span>
-                )}
-              </li>
-            );
-          })}
+          {itemMap.list.map((listItem) => (
+            <li key={listItem.valueKey} className={styles.listItem}>
+              <span>{listItem.title}</span>
+              {listItem.render(item[listItem.valueKey], item)}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
