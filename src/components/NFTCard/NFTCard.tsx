85defcea35b25cb2caa431332ad76afd131f11d8
vladji <vladjislav@gmail.com>
2022-11-24 03:56:43 +0600

    feat: create pool with assets

--- src/components/NFTCard/NFTCard.tsx
+++ src/components/NFTCard/NFTCard.tsx
@@ -14,8 +14,9 @@ interface NFTCardProps {
   selected?: boolean;
   disabled?: boolean;
   simpleCard?: boolean;
+  wholeAreaSelect?: boolean;
   imageUrl: string;
-  name?: string;
+  name: string;
   price?: string;
   onCardClick?: () => void;
   onAddToCart?: () => void;
@@ -27,6 +28,7 @@ export const NFTCard: FC<NFTCardProps> = ({
   selected = false,
   disabled = false,
   simpleCard = false,
+  wholeAreaSelect = false,
   imageUrl,
   name = UNTITLED,
   price,
@@ -40,6 +42,7 @@ export const NFTCard: FC<NFTCardProps> = ({
         styles.card,
         { [styles.cardSelected]: selected },
         { [styles.cardDisabled]: disabled },
+        { [styles.wholeAreaSelect]: !disabled && !selected && wholeAreaSelect },
         className,
       )}
       onClick={onCardClick && onCardClick}
@@ -57,7 +60,7 @@ export const NFTCard: FC<NFTCardProps> = ({
             </Button>
           </div>
         )}
-        {!simpleCard && selected && (
+        {selected && (
           <DeleteButton className={styles.deleteButton} onClick={onAddToCart} />
         )}
         <img className={styles.cardImage} src={imageUrl} alt={name} />
