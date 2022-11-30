3ca3cd95dbc27f2e9c9ac00605b615d6660b81f9
sablevsky <vladislav1145@gmail.com>
2022-11-30 17:53:01 +0100

    Pass rarity prop into NftCard component

--- src/pages/Collection/components/CollectionTabs/CollectionSellTab.tsx
+++ src/pages/Collection/components/CollectionTabs/CollectionSellTab.tsx
@@ -78,6 +78,7 @@ export const CollectionSellTab: FC = () => {
                 onAddToCart={createOnBtnClick(order)}
                 selected={order?.selected}
                 disabled={order.price <= 0}
+                rarity={order.rarity}
               />
             ))}
           </FakeInfinityScroll>
