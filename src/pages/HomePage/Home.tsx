8cebd6458fa6a304334816a08380ad33fd80b008
vladji <vladjislav@gmail.com>
2022-12-01 02:15:00 +0600

    fix: spinner on landing

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -2,10 +2,18 @@ import { FC } from 'react';
 import { AppLayout } from '../../components/Layout/AppLayout';
 import { Stats } from './components/Stats';
 import { TopMarkets } from './components/TopMarkets';
+import { Spinner } from '../../components/Spinner/Spinner';
 
 import styles from './Home.module.scss';
+import { useFetchAllStats, useFetchTVL, useTopMarkets } from './hooks';
 
 export const Home: FC = () => {
+  const { topMarkets, topMarketsLoading } = useTopMarkets();
+  const { allStats, allStatsLoading } = useFetchAllStats();
+  const { TVLstat, TVLStatLoading } = useFetchTVL();
+
+  const isLoading = topMarketsLoading || allStatsLoading || TVLStatLoading;
+
   return (
     <AppLayout>
       <div className={styles.contentWrapper}>
@@ -13,8 +21,14 @@ export const Home: FC = () => {
           the best NFT marketplace on{' '}
           <span className={styles.emphasis}>SOLANA</span> for traders
         </h1>
-        <Stats />
-        <TopMarkets />
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <Stats TVLstat={TVLstat} allStats={allStats} />
+            <TopMarkets topMarkets={topMarkets} />
+          </>
+        )}
       </div>
     </AppLayout>
   );
--- src/pages/HomePage/components/TopMarkets.tsx
+++ src/pages/HomePage/components/TopMarkets.tsx
@@ -1,29 +1,19 @@
 import { FC } from 'react';
-import { useTopMarkets } from '../hooks';
-import { Spinner } from '../../../components/Spinner/Spinner';
 import { MarketCard } from './MarketCard';
+import { TopMarket } from '../../../requests/types';
 
 import styles from './styles.module.scss';
 
-export const TopMarkets: FC = () => {
-  const { topMarkets, topMarketsLoading } = useTopMarkets();
+export const TopMarkets: FC<{ topMarkets: TopMarket[] }> = ({ topMarkets }) => {
   topMarkets.length = 5;
   return (
     <>
-      {topMarketsLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          <h2 className={styles.topMarketsTitle}>
-            All your awesome NFTs are here
-          </h2>
-          <ul className={styles.topMarkets}>
-            {topMarkets.map((market, index) => (
-              <MarketCard key={index} market={market} />
-            ))}
-          </ul>
-        </>
-      )}
+      <h2 className={styles.topMarketsTitle}>All your awesome NFTs are here</h2>
+      <ul className={styles.topMarkets}>
+        {topMarkets.map((market, index) => (
+          <MarketCard key={index} market={market} />
+        ))}
+      </ul>
     </>
   );
 };
