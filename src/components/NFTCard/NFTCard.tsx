80fa2dbbb69f8552283145a1ee553a8e648da512
sablevsky <vladislav1145@gmail.com>
2022-11-30 17:52:21 +0100

    Add Rarity component for NFTCard

--- src/components/NFTCard/NFTCard.tsx
+++ src/components/NFTCard/NFTCard.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 import classNames from 'classnames';
+
 import Button from '../Buttons/Button';
 import DeleteButton from '../Buttons/DeleteButton';
 import { PlusIcon } from '../../icons/PlusIcon';
 import { LoopIcon } from '../../icons/LoopIcon';
 import { SolPrice } from '../SolPrice/SolPrice';
 import { UNTITLED } from '../../constants/common';
-
+import { NftRarity } from '../../state/core/types';
 import styles from './NFTCard.module.scss';
+import HowRareIsIcon from '../../icons/HowRareIsIcon';
+import MoonRankIcon from '../../icons/MoonRankIcon';
 
 interface NFTCardProps {
   className?: string;
@@ -18,6 +21,7 @@ interface NFTCardProps {
   imageUrl: string;
   name: string;
   price?: string;
+  rarity?: NftRarity;
   onCardClick?: () => void;
   onAddToCart?: () => void;
   onExchange?: () => void;
@@ -32,6 +36,7 @@ export const NFTCard: FC<NFTCardProps> = ({
   imageUrl,
   name = UNTITLED,
   price,
+  rarity,
   onCardClick,
   onAddToCart,
   onExchange,
@@ -66,6 +71,7 @@ export const NFTCard: FC<NFTCardProps> = ({
           <DeleteButton className={styles.deleteButton} onClick={onAddToCart} />
         )}
         <img className={styles.cardImage} src={imageUrl} alt={name} />
+        {!!rarity && <Rarity rarity={rarity} />}
       </div>
       <div className={styles.cardContent}>
         <h5 className={styles.cardTitle}>{name}</h5>
@@ -73,6 +79,27 @@ export const NFTCard: FC<NFTCardProps> = ({
           <SolPrice className={styles.cardPrice} price={parseFloat(price)} />
         )}
       </div>
+    </div>
+  );
+};
+
+interface RarityProps {
+  rarity: NftRarity;
+}
+
+const Rarity: FC<RarityProps> = ({ rarity }) => {
+  return (
+    <div className={styles.rarityWrapper}>
+      {!!rarity.howRareIs && (
+        <div className={styles.rarityValue}>
+          <HowRareIsIcon /> {rarity.howRareIs}
+        </div>
+      )}
+      {!!rarity.moonRank && (
+        <div className={styles.rarityValue}>
+          <MoonRankIcon /> {rarity.moonRank}
+        </div>
+      )}
     </div>
   );
 };
