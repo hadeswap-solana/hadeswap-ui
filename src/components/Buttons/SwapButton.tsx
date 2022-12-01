f870cb35f50b23aa95088189385b397ca299d1e0
Vladislav Plaksa <ezekiel.9218@gmail.com>
2022-12-01 17:05:15 +0300

    add disabled swap button

--- src/components/Buttons/SwapButton.tsx
+++ src/components/Buttons/SwapButton.tsx
@@ -1,24 +1,30 @@
 import { FC } from 'react';
+import { useWallet } from '@solana/wallet-adapter-react';
 import classNames from 'classnames';
-import { LoopIcon } from '../../icons/LoopIcon';
-import Button from './Button';
 
+import { LoopIcon } from '../../icons/LoopIcon';
 import styles from './styles.module.scss';
+import Button from './Button';
 
 interface ButtonProps {
   className?: string;
   onClick: (args?: any) => void;
 }
 
-export const SwapButton: FC<ButtonProps> = ({ className, onClick }) => (
-  <div className={styles.swapButtonWrapper}>
-    <Button
-      onClick={onClick}
-      className={classNames(styles.swapButton, className)}
-      outlined
-    >
-      <LoopIcon />
-      <span>swap</span>
-    </Button>
-  </div>
-);
+export const SwapButton: FC<ButtonProps> = ({ className, onClick }) => {
+  const { connected } = useWallet();
+
+  return (
+    <div className={styles.swapButtonWrapper}>
+      <Button
+        onClick={onClick}
+        className={classNames(styles.swapButton, className)}
+        isDisabled={!connected}
+        outlined
+      >
+        <LoopIcon />
+        <span>swap</span>
+      </Button>
+    </div>
+  );
+};
