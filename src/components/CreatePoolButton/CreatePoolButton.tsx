057e85809f312e71b6f0683afc2b57b919ca6760
Alexandr-Horton <aleksandr.horton@gmail.com>
2022-12-09 20:01:16 +0400

    useCall.. -> navLink

--- src/components/CreatePoolButton/CreatePoolButton.tsx
+++ src/components/CreatePoolButton/CreatePoolButton.tsx
@@ -1,9 +1,8 @@
-import { FC, useCallback } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import { FC } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
 import classNames from 'classnames';
 
 import Button from '../Buttons/Button';
-import { createCreatePoolPickSideLink } from '../../constants';
 import styles from './CreatePoolButton.module.scss';
 
 interface CreatePoolButtonProps {
@@ -11,21 +10,13 @@ interface CreatePoolButtonProps {
 }
 
 export const CreatePoolButton: FC<CreatePoolButtonProps> = ({ className }) => {
-  const history = useHistory();
   const { publicKey: marketPublicKey } = useParams<{ publicKey: string }>();
 
-  const onCreatePoolClick = useCallback(() => {
-    marketPublicKey
-      ? history.push(createCreatePoolPickSideLink(marketPublicKey))
-      : history.push('/create-pool');
-  }, [history, marketPublicKey]);
-
   return (
-    <Button
-      className={classNames(styles.poolButton, className)}
-      onClick={onCreatePoolClick}
-    >
-      <span>create pool</span>
-    </Button>
+    <NavLink to={`/create-pool/${marketPublicKey || ''}`}>
+      <Button className={classNames(styles.poolButton, className)}>
+        <span>create pool</span>
+      </Button>
+    </NavLink>
   );
 };
