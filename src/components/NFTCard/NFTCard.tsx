17e9fe77170df745196c9796b8412c0d3a72a242
Alexandr-Horton <aleksandr.horton@gmail.com>
2022-12-14 13:26:46 +0400

    fix card on poolpage

--- src/components/NFTCard/NFTCard.tsx
+++ src/components/NFTCard/NFTCard.tsx
@@ -30,6 +30,7 @@ interface NFTCardProps {
   onAddToCart?: () => void;
   onExchange?: () => void;
   withoutAddToCartBtn?: boolean;
+  createPool?: boolean;
 }
 
 export const NFTCard: FC<NFTCardProps> = ({
@@ -46,6 +47,7 @@ export const NFTCard: FC<NFTCardProps> = ({
   // onAddToCart,
   onExchange,
   withoutAddToCartBtn,
+  createPool = false,
 }) => {
   const { connected } = useWallet();
 
@@ -88,7 +90,12 @@ export const NFTCard: FC<NFTCardProps> = ({
         )}
 
         <div className={styles.cardBtnWrapper}>
-          {!withoutAddToCartBtn && (
+          {!simpleCard && !withoutAddToCartBtn && (
+            <Button outlined className={styles.cardButton}>
+              {selected ? <MinusIcon /> : <PlusIcon />}
+            </Button>
+          )}
+          {createPool && (
             <Button outlined className={styles.cardButton}>
               {selected ? <MinusIcon /> : <PlusIcon />}
             </Button>
