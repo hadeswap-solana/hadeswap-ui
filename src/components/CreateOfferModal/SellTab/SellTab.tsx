22420e4811aa17eafb1449f1269b026c59fefaac
Vladislav Plaksa <ezekiel.9218@gmail.com>
2022-12-21 16:27:37 +0300

    add warning badge

--- src/components/CreateOfferModal/SellTab/SellTab.tsx
+++ src/components/CreateOfferModal/SellTab/SellTab.tsx
@@ -2,15 +2,19 @@ import { FC, useRef } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Form, InputNumber } from 'antd';
+import { Form, InputNumber, Tooltip } from 'antd';
 import {
   BondingCurveType,
   PairType,
 } from 'hadeswap-sdk/lib/hadeswap-core/types';
 
-import { selectAllMarketsLoading } from '../../../state/core/selectors';
+import {
+  selectAllMarkets,
+  selectAllMarketsLoading,
+} from '../../../state/core/selectors';
 import { usePoolServiceAssets } from '../../PoolSettings/hooks/usePoolServiceAssets';
 import { useCreatePool } from '../../../pages/CreatePool/hooks';
+import { NotifyInfoIcon } from '../../../icons/NotifyInfoIcon';
 import { AssetsBlock } from '../../PoolSettings/AssetsBlock';
 import { Spinner } from '../../Spinner/Spinner';
 import styles from './SellTab.module.scss';
@@ -18,8 +22,13 @@ import Button from '../../Buttons/Button';
 
 const SellTab: FC<{ onCancel: () => void }> = ({ onCancel }) => {
   const { publicKey: marketPublicKey } = useParams<{ publicKey: string }>();
-  const marketsLoading = useSelector(selectAllMarketsLoading);
   const { connected } = useWallet();
+
+  const markets = useSelector(selectAllMarkets);
+  const marketsLoading = useSelector(selectAllMarketsLoading);
+  const chosenMarket = markets.find(
+    (market) => market.marketPubkey === marketPublicKey,
+  );
 
   const {
     nfts,
@@ -38,6 +47,8 @@ const SellTab: FC<{ onCancel: () => void }> = ({ onCancel }) => {
   const priceValue: number = Form.useWatch('price', formPrice);
 
   const rawSpotPrice = priceValue * 1e9;
+
+  const isWarningVisible = parseFloat(chosenMarket?.floorPrice) < priceValue;
 
   const { create: onCreatePoolClick } = useCreatePool({
     pairType: PairType.NftForToken,
@@ -78,12 +89,21 @@ const SellTab: FC<{ onCancel: () => void }> = ({ onCancel }) => {
             className={styles.card}
           />
           <div>
-            <h3 className={styles.cardSubTitle}>price</h3>
+            <h3 className={styles.cardSubTitle}>price per NFT</h3>
             <Form form={formPrice} initialValues={initialValuesPrice}>
               <Form.Item name="price">
                 <InputNumber min="0" addonAfter="SOL" />
               </Form.Item>
             </Form>
+          </div>
+          <div className={styles.notifyWrapper}>
+            {isWarningVisible && (
+              <div className={styles.notify}>
+                <NotifyInfoIcon />
+                do you really want to list your items below floor of{' '}
+                {chosenMarket?.floorPrice} SOL?
+              </div>
+            )}
           </div>
           <div className={styles.noticeRow}>
             <span className={styles.noticeTitle}>total</span>
