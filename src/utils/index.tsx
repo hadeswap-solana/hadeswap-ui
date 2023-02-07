847f6b924e981e5262470d8c1a7a5a031729eba9
vladji <vladjislav@gmail.com>
2023-02-07 21:07:48 +0600

    token slippage

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -299,8 +299,8 @@ export const calcTokenAmount = (
   solAmount: string,
   tokenRate: number,
 ): string => {
-  const amount = (Number(solAmount) + 0.005) * tokenRate;
-  return amount.toFixed(2);
+  const amount = Number(solAmount) * tokenRate;
+  return (amount + amount / 100).toFixed(2);
 };
 
 export const PUBKEY_PLACEHOLDER = '11111111111111111111111111111111';
