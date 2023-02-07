9f772aaec236d1bc58dc872ad0b65916a9a34e30
vladji <vladjislav@gmail.com>
2023-02-08 00:07:45 +0600

    fix console errors

--- src/utils/transactions/createDepositLiquidityOnlyBuyOrdersTxns.ts
+++ src/utils/transactions/createDepositLiquidityOnlyBuyOrdersTxns.ts
@@ -1,8 +1,6 @@
 import { WalletContextState } from '@solana/wallet-adapter-react';
 import { web3 } from 'hadeswap-sdk';
 import { chunk } from 'lodash';
-import { SOL_WITHDRAW_ORDERS_LIMIT__PER_TXN } from '../../hadeswap';
-import { getArrayByNumber } from './helpers';
 import { depositLiquidityOnlyBuyOrdersToPair } from 'hadeswap-sdk/lib/hadeswap-core/functions/market-factory/pair/virtual/deposits';
 
 const sendTxnPlaceHolder = async (): Promise<null> =>
