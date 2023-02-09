3872f00b8e41331d234f8742041863238c8073fb
Vedamir Efanov <vedamire@protonmail.com>
2023-02-09 22:45:54 +0400

    moved on creator priority whitelist for now

--- src/components/Layout/helpers.ts
+++ src/components/Layout/helpers.ts
@@ -111,9 +111,9 @@ const createSellNftFromPairIx: CreateIx = async ({
         nftValidationAdapter: new web3.PublicKey(
           order?.nftValidationAdapter || PUBKEY_PLACEHOLDER,
         ),
-        nftValidationAdapterV2:
-          order?.nftValidationAdapterV2 &&
-          new web3.PublicKey(order.nftValidationAdapterV2),
+        nftValidationAdapterV2: order?.nftValidationAdapter
+          ? undefined
+          : new web3.PublicKey(order.nftValidationAdapterV2),
         pair: new web3.PublicKey(pair.pairPubkey),
         userPubkey: walletPubkey,
         protocolFeeReceiver: new web3.PublicKey(
@@ -138,9 +138,9 @@ const createSellNftFromPairIx: CreateIx = async ({
         nftValidationAdapter: new web3.PublicKey(
           order?.nftValidationAdapter || PUBKEY_PLACEHOLDER,
         ),
-        nftValidationAdapterV2:
-          order?.nftValidationAdapterV2 &&
-          new web3.PublicKey(order.nftValidationAdapterV2),
+        nftValidationAdapterV2: order?.nftValidationAdapter
+          ? undefined
+          : new web3.PublicKey(order.nftValidationAdapterV2),
         pair: new web3.PublicKey(pair.pairPubkey),
         userPubkey: walletPubkey,
         protocolFeeReceiver: new web3.PublicKey(
